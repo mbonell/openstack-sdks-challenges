d2b7b51e8ec3e55e334d7d395035e1179da9257e
Marcela Bonell Manjarrez <mbonellm@mbonellm-mac01.local>
2016-06-22 20:50:59 -0500

    Adding Fog => Connect to the cloud challenge

---
+++ fog/challenge-1.rb
@@ -0,0 +1,13 @@
+require "fog/openstack"
+
+#1: Connecting to the cloud
+
+@connection_params = {
+	openstack_auth_url:     "https://YOUR_IDENTITY_CONTROLLER",
+	openstack_username:     "YOUR_USERNAME",
+  	openstack_api_key:      "YOUR_PASSWORD",
+  	openstack_project_name: "YOUR_PROJECT",
+  	openstack_region: 		"YOUR_REGION", 
+}
+
+compute = Fog::Compute::OpenStack.new(@connection_params)
