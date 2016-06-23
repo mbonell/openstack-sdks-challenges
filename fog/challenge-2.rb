9248173b190b54b4849379d814348889165a63e3
Marcela Bonell <marcelabonell@gmail.com>
2016-06-22 21:51:19 -0500

    Adding Fog => Listing images challenge

---
+++ fog/challenge-2.rb
@@ -0,0 +1,16 @@
+require "fog/openstack"
+
+#1: Connecting to the cloud
+
+@connection_params = {
+	openstack_auth_url:     "https://YOUR_IDENTITY_CONTROLLER",
+	openstack_username:     "YOUR_USERNAME",
+  	openstack_api_key:      "YOUR_PASSWORD",
+  	openstack_project_name: "YOUR_PROJECT",
+  	openstack_region: 	"YOUR_REGION", 
+}
+
+compute = Fog::Compute::OpenStack.new(@connection_params)
+
+#2: Listing images 
+p compute.images
