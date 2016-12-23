73f833fc83cfe4c089ceec5f0b6695b60f197801
Marcela Bonell <marcelabonell@gmail.com>
2016-12-23 12:45:20 -0600

    Gophercloud => Update worker vm creation

--- gophercloud/nova/encoder/create_worker.go
+++ gophercloud/nova/encoder/create_worker.go
@@ -7,17 +7,18 @@ import (
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
+	"os"
 	"time"
 )
 
 func main() {
 
 	// Set cloud credentials
-	opts := gophercloud.AuthOptions{
-		IdentityEndpoint: "https://YOUR_IDENTITY_CONTROLLER",
-		Username:         "YOUR_USERNAME",
-		Password:         "YOUR_PASSWORD",
-		DomainName:       "default",
+	opts, err := openstack.AuthOptionsFromEnv()
+
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
 	// Create connection with the cloud
@@ -30,21 +31,24 @@ func main() {
 
 	// Using the compute service with the cloud
 	client, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
-		Region: "RegionOne",
+		Region: os.Getenv("OS_REGION_NAME"),
+		Type:   "computev21",
 	})
 
 	// Create an instance
 	server, err := servers.Create(client, servers.CreateOpts{
-		Name:       "worker-" + time.Now().String(),
-		FlavorName: "m1.medium",
-		ImageName:  "ubuntu-server-16.04.1",
+		Name:           "worker-" + time.Now().String(),
+		FlavorRef:      os.Getenv("WORKER_SERVER_FLAVOR"),
+		ImageRef:       os.Getenv("WORKER_SERVER_IMAGE"),
+		Networks:       []servers.Network{servers.Network{UUID: os.Getenv("WORKER_SERVER_NETWORK")}},
+		SecurityGroups: []string{"worker"},
 	}).Extract()
 
 	if err != nil {
-		fmt.Println("Unable to create server: %s", err)
+		fmt.Printf("Unable to create server: %s", err)
 		return
 	}
 
-	fmt.Println("Server created!, ID: %s", server.ID)
+	fmt.Printf("Server created!, ID: %s", server.ID)
 
 }
