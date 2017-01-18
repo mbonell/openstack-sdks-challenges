5f92be56dc7bae76b5320d42b499f742d14416c4
Marcela Bonell <marcelabonell@gmail.com>
2017-01-18 15:17:08 -0600

    [shade] Adding README in the MongoDB service

--- shade/cinder/mongodb/README.md
+++ shade/cinder/mongodb/README.md
@@ -1,1 +1,57 @@
-TODO
+# MongoDB database service
+The service uses Cinder API to add persistent storage and Nova API to launch a MongoDB server ready to use for your application. The code sample uses [Shade](http://docs.openstack.org/infra/shade/) (Python) as SDK.
+
+## Service flow
+1. Creating the Cinder volume
+  * The persistent volume should be created as first step.
+1. Creating a MongoDB server
+  * Select the image, flavor, network and security groups for the server instance.
+  * Set as env vars the name of your database, user and password.
+  * Through the compute API, launch the MongoDB instance specifying the infrastructure values and user data script.
+1. Adding persistent storage to the MongoDB server
+  * Once the database server is created, the Cinder volume will be attached to it and then the init script will update the dependencies, install MongoDB, format the volume, mount it in the MongoDB data files location and create the database with its user and password.
+
+## Cloud services used
+* Block Storage (Cinder)
+* Compute (Nova)
+
+## Usage
+
+### Pre-requisites
+* Images with cloud-init enabled.
+* Python and Shade installed on your development system.
+
+### Setting the cloud profile to use (clouds.yaml)
+```
+myfavoriteopenstack:
+  auth:
+    auth_url: https://example.com:5000/v3
+    username: admin
+    password: admin
+    project_name: default
+    domain_id: default
+  region_name: RegionOne
+```
+
+### Set the infrastructure values for the database server (flavor, image and network IDs)
+```
+export MONGODB_FLAVOR=3
+export MONGODB_IMAGE=41ba40fd-e801-4639-a842-e3a2e5a2ebdd
+export MONGODB_NETWORK=7004a83a-13d3-4dcd-8cf5-52af1ace4cae
+```
+
+### Set values to configure the database connection string
+```
+export MONGODB_DATABASE=mongo_db
+export MONGODB_USER=admin
+export MONGODB_PASSWORD=secret
+```
+
+### Launching the MongoDB database service
+```
+$ python mongodb/main.py
+```
+At the end the script will provide you the MongoDB URL to use as connection string in your application.
+```
+MONGO_URL='mongodb://admin:secret@<SERVICE_IP>:27017/mongo_db'
+```
--- shade/cinder/mongodb/main.py
+++ shade/cinder/mongodb/main.py
@@ -10,8 +10,16 @@ image_id = os.getenv('MONGODB_IMAGE')
 flavor_id = os.getenv('MONGODB_FLAVOR')
 private_network_id = os.getenv('MONGODB_NETWORK')
 
+mongodb_database = os.getenv('MONGODB_DATABASE')
+mongodb_username = os.getenv('MONGODB_USER')
+mongodb_password = os.getenv('MONGODB_PASSWORD')
+
 if image_id == None or flavor_id == None or private_network_id == None:
-    print('[Error] Please specify the ID of the following values: Image, flavor and external network.')
+    print('[Error] Please specify the ID of the following values: Image, flavor and network.')
+    quit()
+
+if mongodb_database == None or mongodb_username == None or mongodb_password == None:
+    print('[Warning] Please specify the values to configure your database connection string: Database name, username and password.')
     quit()
 
 # Creating a Cinder volume
@@ -24,6 +32,7 @@ else:
     print('Creating security group.')
     conn.create_security_group(sec_group_name, 'network access for mongodb service.')
     conn.create_security_group_rule(sec_group_name, 22, 22, 'TCP')
+    conn.create_security_group_rule(sec_group_name, 27017, 27017, 'TCP')
 
 ex_userdata = '''#!/usr/bin/env bash
 curl -L -s https://raw.githubusercontent.com/MBonell/openstack-sdks-challenges/master/shade/cinder/mongodb/init.sh | bash -s --
@@ -35,11 +44,17 @@ database_server = conn.create_server(wait=True, auto_ip=False,
     image = image_id,
     flavor = flavor_id,
     network = private_network_id,
-    key_name = 'marcela-ws',
+    key_name = 'marcela-ws', # DELETE
     security_groups = [sec_group_name],
     userdata = ex_userdata)
 
 # Adding persistent storage to the database server
 conn.attach_volume(database_server, database_volume, '/dev/vdb')
 
-print('MongoDB service ready!')
+if conn.get_server_public_ip(database_server):
+    ip_service = conn.get_server_public_ip(database_server)
+else:
+    ip_service = conn.get_server_private_ip(database_server)
+
+print("MongoDB service ready!")
+print("Connection string: MONGO_URL='mongodb://" + mongodb_username + ":" + mongodb_password + "@" + ip_service + ":27017/" + mongodb_database + "'")
